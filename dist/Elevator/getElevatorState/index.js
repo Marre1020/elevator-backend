3953637855eb40942d595552d9fea6eaa1740d19
marre1020 <marvinkristusbenno@gmail.com>
2021-04-07 17:23:14 +0200

    removing unwanted files

--- dist/Elevator/getElevatorState/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getElevatorState = void 0;
-const getElevatorState = ({ res, elevatorState }) => {
-    res.send(elevatorState);
-};
-exports.getElevatorState = getElevatorState;
-//# sourceMappingURL=index.js.map
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./Server");
-const Elevator_1 = require("./Elevator");
-const Routes_1 = require("./Routes");
-const app = Server_1.createServer();
-const ElevatorMod = Elevator_1.createElevatorMod({ app });
-Routes_1.createRoutes({ app, ElevatorMod });
-//# sourceMappingURL=index.js.map
